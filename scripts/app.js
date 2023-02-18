ef36c5adaa7032c69ae760e89d8a9a1d929388bd
Amritpal <Amritpal@DESKTOP-IN387AP>
2023-02-17 22:54:29 -0500

    ICE03

--- scripts/app.js
+++ scripts/app.js
@@ -23,6 +23,17 @@
     function DisplayHomePage(){
         console.log("Display Home Page called");
 
+        let xhr = new XMLHttpRequest();
+
+        xhr.addEventListener("readystatechange", () => {
+            if( xhr.readyState === 4 && xhr.status === 200){
+
+                //console.log(xhr.responseText);
+                $("header").html(xhr.responseText);
+            }
+        });
+
+        xhr.open("GET", "header.html");
         $("#AboutUsBtn").on("click", () => {
             location.href = "about.html"
         });
@@ -31,6 +42,31 @@
         <p id="ArticleParagraph" class="mt-3">This is Article paragraph</p>
         </article>`)
 
+    }
+
+    function AjaxRequest(method,url,callback){
+        let XHR =  new XMLHttpRequest();
+        XHR.addEventListener(("readystatechange"),()=>{
+            if(XHR.readyState === 4 && XHR.status ===200)
+            {
+                if(typeof callback === "function"){
+                    callback(XHR.responseText);
+
+                }else {
+                    console.log("Error: callback not a function");
+                }
+            }
+        });
+        XHR.open(method,url);
+        XHR.send();
+
+    }
+
+    function LoadHeader(html_data)
+    {
+        $("header").html(html_data);
+        $(`li>a:contains(${document.title})`).addClass("active");
+        CheckLogin();
     }
 
 
@@ -48,8 +84,103 @@
         console.log("Display About Page called");
 
     }
+
+    function DisplayRegisterPage(){
+        console.log("Display Register Page called");
+
+        $("#submitButton").on("click",(event)=>{
+            event.preventDefault();
+        });
+    }
+
+    function DisplayLoginPage(){
+        console.log("Display Login Page called");
+
+        let messageArea= $("#messageArea");
+        messageArea.hide();
+        $("#loginButton").on("click",function (){
+
+            let success = false;
+            let newUser = new core.User();
+            $.get("./data/user.json",function (data){
+                for(const user of data.users){
+                    if(username.value === user.Username && password.value === user.Password){
+                        newUser.fromJSON(user);
+                        success=true;
+                        break;
+                    }
+                }
+
+                if(success){
+                    sessionStorage.setItem("user",newUser.serialize());
+                    messageArea.removeAttr("class").hide();
+                    location.href="contact-list.html";
+                }else {
+                    $("#username").trigger("focus").trigger("select");
+                    messageArea.addClass("alert alert-danger").text("user is not exists").show();
+
+                }
+            });
+        });
+        $("#cancelButton").on("click",function (){
+            document.forms[0].reset();
+            location.href ="index.html";
+        });
+
+
+    }
+
+    function CheckLogin(){
+        if(sessionStorage.getItem("user")) {
+
+            $("#login").html(`<a id="logout" class="nav-link" href="#"><i class="fas fa-sign-out-alt "></i>Logout</a>`)
+        }
+        $("#logout").on("click",function () {
+            sessionStorage.clear();
+            location.href = "index.html";
+
+        });
+    }
+
+
+
+    function ValidateField(input_field_id, regular_expression, error_message){
+        let messageArea = $("#messageArea");
+
+        $(input_field_id).on("blur", function(){
+
+            let inputFieldText = $(this).val();
+
+            if(!regular_expression.test(inputFieldText)){
+                //fail validation
+                $(this).trigger("focus").trigger("select");
+                messageArea.addClass("alert alert-danger").text(error_message).show();
+            }
+            else{
+                //pass validation
+                messageArea.removeAttr("class").hide();
+
+            }
+
+
+
+        });
+
+    }
+
+    function ContactFormValidation(){
+        ValidateField("#fullName", /^([A-Z][a-z]{1,3}\.?\s)?([A-Z][a-z]+)+([\s,-]([A-z][a-z]+))*$/, "Please enter a valid firstname and last name (ex Mr. Peter Parker)");
+        ValidateField("#contactNumber", /^(\+\d{1,3}[\s-.])?\(?\d{3}\)?[\s-.]?\d{3}[\s-.]\d{4}$/, "Please enter a valid contact number (ex 555-555-4567");
+
+        ValidateField("#emailAddress", /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]{2,10}$/, "Please enter a valid email address (ex peter.parker@isp.com)");
+    }
     function DisplayContactUsPage(){
         console.log("Display Contact Page called");
+
+        ContactFormValidation();
+
+
+
         let sendButton = document.getElementById("sendButton");
         let subscribeCheckbox = document.getElementById("subscribeCheckbox");
 
@@ -120,6 +251,7 @@
         function DisplayEditPage() {
             console.log("Edit Contact Page ");
 
+            ContactFormValidation();
 
             let page = location.hash.substring(1);
             switch (page) {
@@ -162,7 +294,8 @@
 
     function Start()
     {
-        console.log("Application Started!")
+        console.log("Application Started!");
+        AjaxRequest("GET","header.html",LoadHeader);
         switch(document.title)
         {
             case "Home":
@@ -185,6 +318,10 @@
                 break;
             case "Edit Contact":
                 DisplayEditPage();
+            case "Register":
+                DisplayRegisterPage();
+            case "Login":
+                DisplayLoginPage();
         }
 
 
